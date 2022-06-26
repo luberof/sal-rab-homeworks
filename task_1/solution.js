120cb21bef4597fce3009883429e5b6577413cc7
Lubov Yerofeyeva <luberof@gmail.com>
2022-06-26 09:46:51 +0500

    task 1.2 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -26,6 +26,7 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue = oldValue + 1;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
