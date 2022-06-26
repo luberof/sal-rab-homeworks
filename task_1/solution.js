9bc6de972ec57d75c3d05f637ea9cc134eb3b969
Lubov Yerofeyeva <luberof@gmail.com>
2022-06-26 09:40:05 +0500

    task 1.1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,6 +1,9 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    let message;
+    message = productName + " " + "за" + " " + productPrice + " " + "теперь в корзине!";
+
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
