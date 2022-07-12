6119584a3cae6a340dda9e16183c228c2b5c4168
Lubov Yerofeyeva <luberof@gmail.com>
2022-07-12 20:40:20 +0500

    кодстайл поправлен

--- task_4/solution.js
+++ task_4/solution.js
@@ -10,7 +10,7 @@ function parseProducts(json) {
     let data = JSON.parse(json);
     let products = data.products; 
     return products; 
-    }
+}
 
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
@@ -19,9 +19,9 @@ function renderProductsCards(json) {
     clearProducts();
     let products = parseProducts(json);
     length = products.length;
-    for (let i = 0; i < length; i += 1)
-    {
-    addProduct(products[i]);}
+    for (let i = 0; i < length; i += 1) {
+    addProduct(products[i]);
+    }
 } 
 
 // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
